f48e528b96be6759b703b863e089393baa80db04
Taras Haidar <gajdartaras@gmail.com>
2019-07-24 00:09:01 +0200

    Create a React app and publish it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <BrowserRouter>< App /></BrowserRouter>,
+    <HashRouter >< App /></HashRouter >,
     document.getElementById('root')
 );
 serviceWorker.unregister();
