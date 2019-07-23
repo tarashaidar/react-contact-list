c7a1f8768d2833cd895a25c1effd95a3590b9993
Taras Haidar <gajdartaras@gmail.com>
2019-07-23 23:55:37 +0200

    Updates

---
+++ precache-manifest.d64d63c5d0f7bfcfb9a27dcfac9c16d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b324dd52f3ecf44b51c5be4ac0813c63",
+    "url": "/react-contact-list/index.html"
+  },
+  {
+    "revision": "b04903264fbdcd7290c2",
+    "url": "/react-contact-list/static/css/main.7837d61b.chunk.css"
+  },
+  {
+    "revision": "43499cb0e73445959623",
+    "url": "/react-contact-list/static/js/2.cb89b741.chunk.js"
+  },
+  {
+    "revision": "b04903264fbdcd7290c2",
+    "url": "/react-contact-list/static/js/main.e308fa3f.chunk.js"
+  },
+  {
+    "revision": "7395d3b463943f52f692",
+    "url": "/react-contact-list/static/js/runtime~main.784bec08.js"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/react-contact-list/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "32cee4372a4c9744f7e703013278c61d",
+    "url": "/react-contact-list/static/media/cancel.32cee437.svg"
+  },
+  {
+    "revision": "e24cad8280195f088bd3bfb93f09e582",
+    "url": "/react-contact-list/static/media/person-add.e24cad82.svg"
+  },
+  {
+    "revision": "198acd46307369567e1e29a42be518d9",
+    "url": "/react-contact-list/static/media/person.198acd46.svg"
+  },
+  {
+    "revision": "b884e292f0f2f394e31cd93106b0386e",
+    "url": "/react-contact-list/static/media/search.b884e292.svg"
+  }
+]);
